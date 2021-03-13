5811025f12a5d7cfc1649bf8b7fb1f4bf2f3608d
Tamer Almurshidi <taalmurshidi@gmail.com>
2021-03-13 22:57:26 +0100

    Add EventsPage, EventsPageViewModel and StateControl

---
+++ EventsXamarin/EventsXamarin/Views/Popup/DeleteConfirmationPopupPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Prism.Commands;
+
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EventsXamarin.Views.Popup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DeleteConfirmationPopupPage : PopupPage
+    {
+        public DelegateCommand CancelBtnCommand { get; set; }
+        public DelegateCommand DeleteBtnCommand { get; set; }
+
+        public EventHandler<bool> DeleteAction;
+
+        private async void CancelBtnAsync()
+        {
+            await ClosePopupAsync();
+        }
+
+        private async void DeleteBtnConfirmationAsync()
+        {
+            DeleteAction?.Invoke(this, true);
+            await ClosePopupAsync();
+        }
+
+        private async Task ClosePopupAsync()
+        {
+            await PopupNavigation.Instance.PopAsync();
+        }
+
+        public DeleteConfirmationPopupPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            DeleteBtnCommand = new DelegateCommand(DeleteBtnConfirmationAsync);
+            CancelBtnCommand = new DelegateCommand(CancelBtnAsync);
+        }
+    }
+}
