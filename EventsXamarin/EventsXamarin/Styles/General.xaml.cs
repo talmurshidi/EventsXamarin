a9d7549fb78cec3374819118f9c38607486a94bb
Tamer Almurshidi <taalmurshidi@gmail.com>
2021-03-13 20:22:18 +0100

    Add general style with colors schema

---
+++ EventsXamarin/EventsXamarin/Styles/General.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EventsXamarin.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class General : ResourceDictionary
+    {
+        public General()
+        {
+            InitializeComponent();
+        }
+    }
+}
