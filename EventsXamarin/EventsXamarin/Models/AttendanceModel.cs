4e53e65e01d1e89cd45e229d3340b2a9355044d2
Tamer Almurshidi <taalmurshidi@gmail.com>
2021-03-15 14:39:15 +0100

    Update json file data and add extra property. Update model's classes

--- EventsXamarin/EventsXamarin/Models/AttendanceModel.cs
+++ EventsXamarin/EventsXamarin/Models/AttendanceModel.cs
@@ -8,6 +8,9 @@ namespace EventsXamarin.Models
 {
     public class AttendanceModel : ModelBase
     {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
         [JsonProperty("name")]
         public string Name { get; set; }
     }
